78d3dae832e0f231b14acb58ba8854891b3063ee
leeyeun <dpdms0109@naver.com>
2022-06-23 23:47:21 +0900

    useEffect사용

--- blog-page/src/App.jsx
+++ blog-page/src/App.jsx
@@ -3,12 +3,18 @@ import Homepage from "./components/pages/Homepage";
 import PostView from "./components/PostView/PostView";
 import "./reset.css";
 import "./global.css";
-import { data } from "../src/data/data";
+// import { data } from "../src/data/data";
+import { useEffect } from "react";
+import axios from "axios";
 function App() {
+  useEffect(() => {
+    axios.get("");
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
-        <Route exact path="/" component={Homepage}></Route>
+        <Homepage />
+        {/* <Route exact path="/" component={Homepage}></Route> */}
         {/* <Route exact path="/postview" component={PostView}></Route> */}
         <Route exact path="/postview/:id" render={() => <PostView />}></Route>
       </BrowserRouter>
--- blog-page/src/components/pages/Homepage.jsx
+++ blog-page/src/components/pages/Homepage.jsx
@@ -2,19 +2,42 @@ import Banner from "../Banner/Banner";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import PostList from "../PostList/PostList";
-import { data } from "../../data/data";
-import React from "react";
+// import { data } from "../../data/data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 export default function Homepage() {
-  console.log("data", data);
+  const [postsData, setPostData] = useState([]);
+  const [datas, setDatas] = useState(false);
+  // console.log("data", data);
+  useEffect(() => {
+    axios
+      .get("/data.json")
+      .then((res) => {
+        setPostData(res.data);
+        console.log(postsData);
+      })
+      .catch((err) => console.log(err));
+
+    // console.log(postsData);
+  }, [datas]);
+
+  const datalist = () => {
+    // axios
+    //   .get("/data.json")
+    //   .then((res) => setPostData(res.data))
+    //   .catch((err) => console.log(err));
+    // console.log(postsData);
+  };
+
   return (
     <div>
       <Header />
-      {data.map((item) => (
+      {/* {data.map((item) => (
         <React.Fragment key={item}>
           <Banner blog={item.blog} />
           <PostList posts={item.posts} />
         </React.Fragment>
-      ))}
+      ))} */}
 
       <Footer />
     </div>
